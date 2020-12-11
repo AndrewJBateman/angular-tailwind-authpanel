34d0c569c3e2a3a93fc49f965aa83f42a5576c5b
Andrew Bateman <gomezbateman@yahoo.com>
2020-12-11 12:55:08 +0100

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-tailwind-authpanel app is running!');
+    expect(compiled.querySelector('.signin-heading').textContent).toContain('Sign in to your account');
   });
 });
